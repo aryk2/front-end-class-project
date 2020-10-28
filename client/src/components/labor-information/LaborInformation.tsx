4a126cef085a919d4d51456701df5f045461ce6a
tgomez22 <gomez22@pdx.edu>
2020-10-28 15:26:51 -0700

    I think that the laborinformation/unemployment chart is mostly done. It may need to be resized later to make it fit with the other components

--- client/src/components/labor-information/LaborInformation.tsx
+++ client/src/components/labor-information/LaborInformation.tsx
@@ -1,4 +1,5 @@
 import React, { FunctionComponent, useState, useEffect } from "react";
+import UnemploymentChart from "../unemployment-chart";
 const laborKey = process.env.REACT_APP_BLS_KEY;
 const laborUrl =
   "https://api.bls.gov/publicAPI/v2/timeseries/data/LNS14000000?registrationkey=" +
@@ -10,35 +11,35 @@ export const LaborInformation: FunctionComponent<LaborInformationProps> = (
   props
 ) => {
   const [isLoaded, setLoading] = useState(false);
-  const [sept2020, setSept2020] = useState(null);
-  const [aug2020, setAug2020] = useState(null);
-  const [july2020, setJuly2020] = useState(null);
-  const [june2020, setJune2020] = useState(null);
-  const [may2020, setMay2020] = useState(null);
-  const [april2020, setApril2020] = useState(null);
-  const [march2020, setMarch2020] = useState(null);
-  const [feb2020, setFeb2020] = useState(null);
-  const [jan2020, setJan2020] = useState(null);
+  const [sept2020, setSept2020] = useState("");
+  const [aug2020, setAug2020] = useState("");
+  const [july2020, setJuly2020] = useState("");
+  const [june2020, setJune2020] = useState("");
+  const [may2020, setMay2020] = useState("");
+  const [april2020, setApril2020] = useState("");
+  const [march2020, setMarch2020] = useState("");
+  const [feb2020, setFeb2020] = useState("");
+  const [jan2020, setJan2020] = useState("");
 
-  const [sept2019, setSept2019] = useState(null);
-  const [aug2019, setAug2019] = useState(null);
-  const [july2019, setJuly2019] = useState(null);
-  const [june2019, setJune2019] = useState(null);
-  const [may2019, setMay2019] = useState(null);
-  const [april2019, setApril2019] = useState(null);
-  const [march2019, setMarch2019] = useState(null);
-  const [feb2019, setFeb2019] = useState(null);
-  const [jan2019, setJan2019] = useState(null);
+  const [sept2019, setSept2019] = useState("");
+  const [aug2019, setAug2019] = useState("");
+  const [july2019, setJuly2019] = useState("");
+  const [june2019, setJune2019] = useState("");
+  const [may2019, setMay2019] = useState("");
+  const [april2019, setApril2019] = useState("");
+  const [march2019, setMarch2019] = useState("");
+  const [feb2019, setFeb2019] = useState("");
+  const [jan2019, setJan2019] = useState("");
 
-  const [sept2018, setSept2018] = useState(null);
-  const [aug2018, setAug2018] = useState(null);
-  const [july2018, setJuly2018] = useState(null);
-  const [june2018, setJune2018] = useState(null);
-  const [may2018, setMay2018] = useState(null);
-  const [april2018, setApril2018] = useState(null);
-  const [march2018, setMarch2018] = useState(null);
-  const [feb2018, setFeb2018] = useState(null);
-  const [jan2018, setJan2018] = useState(null);
+  const [sept2018, setSept2018] = useState("");
+  const [aug2018, setAug2018] = useState("");
+  const [july2018, setJuly2018] = useState("");
+  const [june2018, setJune2018] = useState("");
+  const [may2018, setMay2018] = useState("");
+  const [april2018, setApril2018] = useState("");
+  const [march2018, setMarch2018] = useState("");
+  const [feb2018, setFeb2018] = useState("");
+  const [jan2018, setJan2018] = useState("");
 
   const getData = async () => {
     await fetch(laborUrl)
@@ -92,7 +93,37 @@ export const LaborInformation: FunctionComponent<LaborInformationProps> = (
   }, []);
 
   if (isLoaded === true) {
-    return <h1></h1>;
+    return (
+      <UnemploymentChart
+        jan2018={jan2018}
+        feb2018={feb2018}
+        march2018={march2018}
+        april2018={april2018}
+        may2018={may2018}
+        june2018={june2018}
+        july2018={july2018}
+        aug2018={aug2018}
+        sept2018={sept2018}
+        jan2019={jan2019}
+        feb2019={feb2019}
+        march2019={march2019}
+        april2019={april2019}
+        may2019={may2019}
+        june2019={june2019}
+        july2019={july2019}
+        aug2019={aug2019}
+        sept2019={sept2019}
+        jan2020={jan2020}
+        feb2020={feb2020}
+        march2020={march2020}
+        april2020={april2020}
+        may2020={may2020}
+        june2020={june2020}
+        july2020={july2020}
+        aug2020={aug2020}
+        sept2020={sept2020}
+      />
+    );
   } else {
     return <h1></h1>;
   }
