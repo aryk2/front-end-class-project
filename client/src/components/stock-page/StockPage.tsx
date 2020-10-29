0664894811cb4289b0e7b4e586a5677277a3e165
tgomez22 <gomez22@pdx.edu>
2020-10-29 12:38:10 -0700

    Extracted open, close, high, low for any stock

--- client/src/components/stock-page/StockPage.tsx
+++ client/src/components/stock-page/StockPage.tsx
@@ -5,24 +5,28 @@ export interface StockPageProps {
   stock: string;
 }
 
-const apiKey = process.env.REACT_APP_FINNHUB_KEY;
+const apiKey = process.env.REACT_APP_ALPHA_VANTAGE_KEY;
 
 export const StockPage: FunctionComponent<StockPageProps> = (props) => {
   const [stock, setStock] = useState(props.stock);
   const [isLoaded, setLoaded] = useState(false);
-
-  let timestamp = new Date();
-  const currentTime = timestamp.getTime();
-  const oneMonthAgo = currentTime - 2629743;
+  const [open, setOpen] = useState<any>([]);
+  const [close, setClose] = useState<any>([]);
+  const [high, setHigh] = useState<any>([]);
+  const [low, setLow] = useState<any>([]);
 
   const endpoint =
-    "https://finnhub.io/api/v1/stock/candle?symbol=" +
+    "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=" +
     stock +
-    "&resolution=M&from=" +
-    currentTime +
-    "&to=" +
-    oneMonthAgo +
-    "&token=";
+    "&outputsize=compact" +
+    "&apikey=" +
+    apiKey;
+
+  const openExp = new RegExp(/(open":")[\d.]+"/, "g");
+  const highExp = new RegExp(/(close":")[\d.]+"/, "g");
+  const lowExp = new RegExp(/(low":")[\d.]+"/, "g");
+  const closeExp = new RegExp(/(close":")[\d.]+"/, "g");
+  const numExp = new RegExp(/[\d.]+/, "g");
 
   const getData = async (endpoint: string) => {
     await fetch(endpoint)
@@ -30,6 +34,35 @@ export const StockPage: FunctionComponent<StockPageProps> = (props) => {
       .then((jsonResponse) => {
         setLoaded(true);
         console.log(jsonResponse);
+        let temp = JSON.stringify(jsonResponse);
+        let openTemp = temp.match(openExp);
+        let highTemp = temp.match(highExp);
+        let lowTemp = temp.match(lowExp);
+        let closeTemp = temp.match(closeExp);
+
+        let openArr = [];
+        for (let i = 0; i < openTemp!.length; ++i) {
+          openArr.push(openTemp![i].match(numExp));
+        }
+
+        let highArr = [];
+        for (let i = 0; i < highTemp!.length; ++i) {
+          highArr.push(highTemp![i].match(numExp));
+        }
+
+        let lowArr = [];
+        for (let i = 0; i < lowTemp!.length; ++i) {
+          lowArr.push(lowTemp![i].match(numExp));
+        }
+
+        let closeArr = [];
+        for (let i = 0; i < closeTemp!.length; ++i) {
+          closeArr.push(closeTemp![i].match(numExp));
+        }
+        setOpen(openArr);
+        setClose(closeArr);
+        setLow(lowArr);
+        setHigh(highArr);
       })
       .catch((error: any) => {
         console.error(error);
@@ -45,7 +78,12 @@ export const StockPage: FunctionComponent<StockPageProps> = (props) => {
     }
   }, []);
 
-  return <h1></h1>;
+  if (isLoaded === true) {
+    //console.log(data);
+    return <h1>We got it</h1>;
+  } else {
+    return <h1>Nope</h1>;
+  }
 };
 
 export default StockPage;
