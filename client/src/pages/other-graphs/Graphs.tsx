2c5f214524c9bb5f8070902e763d1e02c553018a
aryeh <akempler-delugach@ignw.io>
2020-11-02 09:15:35 -0800

    router and nav is working

---
+++ client/src/pages/other-graphs/Graphs.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Layout from '../../components/layout'
+
+// @ts-ignore
+
+export const Graphs = () => {
+  return (
+    <Layout>
+      other graphs
+    </Layout>
+
+  )
+}
+
+export default Graphs
