f089b5db3308ff7651efae57edb84a0f50b5ba90
aryeh <akempler-delugach@ignw.io>
2020-11-02 08:28:29 -0800

    adding layout

---
+++ client/src/components/layout/Layout.stories.tsx
@@ -0,0 +1,26 @@
+import React from "react"
+// also exported from '@storybook/react' if you can deal with breaking changes in 6.1
+import { Meta, Story } from "@storybook/react/types-6-0"
+import { Layout as Component } from "./Layout"
+
+const storybookContainerStyle = {
+  padding: '2em'
+}
+
+export default {
+  title: "Layout",
+  component: Component,
+  argTypes: {},
+  decorators: [
+    (Story) => (
+      <div style={storybookContainerStyle}>
+        <Story />
+      </div>
+    ),
+  ],
+} as Meta
+
+const Template: Story<any> = (args) => <Component {...args} />
+
+export const Layout = Template.bind({})
+Layout.args = {}
